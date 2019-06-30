7927bb5ea15acf3020dda455890ad390875a59df
Mohib <www.muhibkhan@gmail.com>
2019-06-30 18:32:09 +0530

    Java assignment updated

--- CoreJava/src/com/testyantra/assignment/finterfacelamdaimpl/StudentEnumIMPL.java
+++ CoreJava/src/com/testyantra/assignment/finterfacelamdaimpl/StudentEnumIMPL.java
@@ -2,6 +2,9 @@ package com.testyantra.java.finterfacelamdaimpl;
 
 import java.util.ArrayList;
 
+import lombok.extern.java.Log;
+
+@Log
 public class StudentEnumIMPL {
 
 	public static void main(String[] args) {
@@ -9,13 +12,8 @@ public class StudentEnumIMPL {
 		Student s1 = new Student("mohib", 12, 42, 25.5, 'm');
 		Student s2 = new Student("anita", 42, 55, 55.5, 'f');
 		Student s3 = new Student("ankith", 36, 65, 65.5, 'm');
-		Student s4 = new Student("ramesh", 25, 69, 67.5, 'm');
+		Student s4 = new Student("monika", 25, 69, 67.5, 'f');
 		Student s5 = new Student("harish", 22, 71, 75.5, 'm');
-
-		Gender g = Gender.MALE;
-		Gender g2 = Gender.FEMALE;
-
-		char ch = g.getGender();
 
 		ArrayList<Student> al = new ArrayList<>();
 		al.add(s1);
@@ -23,5 +21,15 @@ public class StudentEnumIMPL {
 		al.add(s3);
 		al.add(s4);
 		al.add(s5);
+
+		for (Student s : al) {
+			if (s.getGender() == 'm') {
+				s.name = "Mr. " + s.name;
+			} else {
+				s.name = "Ms. " + s.name;
+			}
+		}
+
+		al.stream().forEach(a -> log.info("" + a));
 	}
 }
