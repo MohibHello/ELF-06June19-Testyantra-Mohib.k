53f818113221b78b687c1ced66d8a97a7725ecae
Mohib Khan <www.muhibkhan@gmail.com>
2019-07-09 18:42:09 +0530

    DesignPattern updated

---
+++ designpattern/src/main/java/com/tyss/sample/MyimmutableClass.java
@@ -0,0 +1,13 @@
+package com.tyss.sample;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public final class MyimmutableClass {
+
+	private String name;
+	private int age;
+	private long phone;
+}
