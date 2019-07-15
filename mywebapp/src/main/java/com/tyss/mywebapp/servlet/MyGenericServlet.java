a545680007becc35fc2f7e8be049a394d7e2fde6
Mohib Khan <www.muhibkhan@gmail.com>
2019-07-15 17:51:32 +0530

    app updated

---
+++ mywebapp/src/main/java/com/tyss/mywebapp/servlet/MyGenericServlet.java
@@ -0,0 +1,39 @@
+package com.tyss.mywebapp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/mygeneric", initParams = { @WebInitParam(name = "actress", value = "basanthi") })
+public class MyGenericServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		ServletContext ctx = getServletContext();
+		String movie = ctx.getInitParameter("movie");
+
+		ServletConfig config = getServletConfig();
+		String actorName = config.getInitParameter("actor");
+		String actressName = config.getInitParameter("actress");
+
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h2>");
+		out.println("hello to you sir =>" + movie + " actor :" + actorName);
+		out.println("<br>");
+		out.println("actress :" + actressName + "<br>");
+		out.println("</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
---
+++ mywebapp/src/main/java/com/tyss/mywebapp/servlet/ServletSearch.java
@@ -0,0 +1,62 @@
+package com.tyss.mywebapp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.testyantra.beans.EmployeeInfoBean;
+import com.testyantra.mywebapp.dao.EmployeeDAO;
+import com.testyantra.mywebapp.dao.EmployeeDAOFactory;
+
+@WebServlet("/search")
+//@WebServlet("/search/employeesearch")
+public class ServletSearch extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String idValue = req.getParameter("id");
+
+		// Interact withDb & get the Employee Info
+		EmployeeDAO dao = EmployeeDAOFactory.getInstance();
+		EmployeeInfoBean bean = dao.getEmployeeInfo(idValue);
+
+		// Send the Response to browser
+		PrintWriter out = resp.getWriter();
+
+		if (bean == null) {
+			out.print("<html>");
+			out.print("<body>");
+			out.print("<h1><span style=\"color:red\">Employee not Found !!!</span></h1>");
+			out.print("<br>");
+			out.print("<body>");
+			out.print("<html>");
+		} else {
+			out.print("<html>");
+			out.print("<body>");
+			out.print("<h1><span style=\"color:green\">Employee Found !!!</span></h1>");
+			out.print("<h3><br>");
+			out.print("<br> ID 		        ---" + bean.getId());
+			out.print("<br> NAME			    ---" + bean.getName());
+			out.print("<br> AGE 			    ---" + bean.getAge());
+			out.print("<br> GENDER			---" + bean.getGender());
+			out.print("<br> SALARY  		    ---" + bean.getSalary());
+			out.print("<br> PHONE  		  	---" + bean.getPhone());
+			out.print("<br> JOINING_DATE	    ---" + bean.getJoiningDate());
+			out.print("<br> ACCCOUNT_NUMBER   ---" + bean.getAccountNumber());
+			out.print("<br> EMAIL  			---" + bean.getEmail());
+			out.print("<br> DESIGNATION    ---" + bean.getDesignation());
+			out.print("<br> DOB        		---" + bean.getDob());
+			out.print("<br> DEPT_NO (FK)   	---" + bean.getDepartmentId());
+			out.print("<br> MGR_ID			---" + bean.getManagerId());
+			out.print("</h3>");
+			out.print("<body>");
+			out.print("<html>");
+		}
+	}
+}
