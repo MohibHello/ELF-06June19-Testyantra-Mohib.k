c08ff4868af4ebdc9d436c624f62588a86ebda6f
Mohib Khan <www.muhibkhan@gmail.com>
2019-07-17 18:57:40 +0530

    JavaScript assignment

---
+++ Web Tech/Web Assignment/JS Assignment/img.js
@@ -0,0 +1,7 @@
+function mouseOverImage() {
+    document.getElementById("A").src = "B.png";
+}
+
+function mouseOutImage() {
+    document.getElementById("A").src = "D.png";
+}
---
+++ Web Tech/Web Assignment/JS Assignment/validaedata.js
@@ -0,0 +1,23 @@
+
+  function checkForm(form)
+  {
+    // validation fails if the input is blank
+    if(form.usrname.value == "") {
+      alert("Error: Input is empty!");
+      form.usrname.focus();
+      return false;
+    }
+
+    // regular expression to match only alphanumeric characters and spaces
+    var re = /^[\w ]+$/;
+
+    // validation fails if the input doesn't match our regular expression
+    if(!re.test(form.usrname.value)) {
+      alert("Error: Input contains invalid characters!");
+      form.usrname.focus();
+      return false;
+    }
+
+    // validation was successful
+    return true;
+  }
