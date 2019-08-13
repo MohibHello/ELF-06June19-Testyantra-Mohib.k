9881ade62751061f25d01f673d13a7c56c5213e8
Mohib Khan <www.muhibkhan@gmail.com>
2019-08-13 19:38:57 +0530

    React project updated

--- Web Tech/ReactJS/web-app/src/components/CreateAccount.js
+++ Web Tech/ReactJS/web-app/src/components/CreateAccount.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Axios from 'axios';
 
 export class CreateAccount extends Component {
 
@@ -9,6 +10,7 @@ export class CreateAccount extends Component {
             email : '',
             phoneno : '',
             password : '',
+            
 
         }
         this.postData = this.postData.bind(this);
@@ -16,30 +18,87 @@ export class CreateAccount extends Component {
 postData(event) {
 event.preventDefault();
 console.log('Post Date',this.state);
+let accountData = this.state;
+Axios.post('https://emp-web-9a0cd.firebaseio.com/accounts.json',accountData).then((response)=>{
+  console.log('Response object',response);
+  this.setState({
+    name : '',
+    email : '',
+    phoneno : '',
+    password : '',
+
+})
+}).catch((error)=>{
+  console.log('error',error);
+})
 }
+
+//form validation
+handleChange = () => {
+  event.preventDefault();
+  const { name, value } = event.target;
+  let errors = this.state.errors;
+
+  switch (name) {
+    case 'name': 
+      errors.name = 
+        value.length < 5
+          ? ' Name must be 5 characters long!'
+          : '';
+      break;
+    case 'email': 
+      errors.email = 
+        value.length<10
+          ? ''
+          : 'Email is not valid!';
+      break;
+    case 'password': 
+      errors.password = 
+        value.length < 8
+          ? 'Password must be 8 characters long!'
+          : '';
+      break;
+      case 'phoneno': 
+      errors.password = 
+        value.length < 10
+          ? 'Password must be 8 characters long!'
+          : '';
+      break;
+    default:
+      break;
+  }
+
+  this.setState({errors, [name]: value}, ()=> {
+      console.log(errors)
+  })
+}
+
+
+
+
 
     render() {
         return (
             
-                             <form onSubmit={this.postData}>
-  <div class="row">
-    <div class="col">
-      <input type="text" value={this.state.name}  onChange={(event)=>{this.setState({name:event.target.value})}}  class="form-control" placeholder="Name"/>
+<form onSubmit={this.handleChange()}>   //change postData
+  <div className="row">
+    <div className="col">
+      <input type="text" name="name"  value={this.state.name}  onChange={(event)=>{this.setState({name:event.target.value})}}  className="form-control" placeholder="Name"/>
     </div>
-    <div class="col">
-      <input type="text" value={this.state.email}  onChange={(event)=>{this.setState({email:event.target.value})}}  class="form-control" placeholder="Email"/>
+    <div className="col">
+      <input type="text" name="email" value={this.state.email}  onChange={(event)=>{this.setState({email:event.target.value})}}  className="form-control" placeholder="Email"/>
   </div>
   </div>
-  <div class="row">
-    <div class="col">
-      <input type="text" class="form-control"  onChange={(event)=>{this.setState({phoneno:event.target.value})}}  value={this.state.phoneno} placeholder="Phone Number"/>
+  <div className="row">
+    <div className="col">
+      <input type="text" name="phoneno" className="form-control"  onChange={(event)=>{this.setState({phoneno:event.target.value})}}  value={this.state.phoneno} placeholder="Phone Number"/>
     </div>
-    <div class="col">
-      <input type="text" class="form-control"  onChange={(event)=>{this.setState({password:event.target.value})}}  value={this.state.password} placeholder="Password"/>
+    <div className="col">
+      <input type="password" name="password" className="form-control"  onChange={(event)=>{this.setState({password:event.target.value})}}  value={this.state.password} placeholder="Password"/>
     </div>
   </div>
   <div>
-      <button> Create Employee</button>
+      <button type="submit"> Create Employee</button>
   </div>
 </form>
             
--- Web Tech/ReactJS/web-app/src/components/ViewAccount.js
+++ Web Tech/ReactJS/web-app/src/components/ViewAccount.js
@@ -1,42 +1,151 @@
 import React, { Component } from 'react'
+import Axios from 'axios'
+import { Modal, Button } from 'react-bootstrap'
 
 export class ViewAccount extends Component {
-    render() {
-        return (
-            <div>
-                <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">PhoneNo</th>
-      <th scope="col">Password</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-            </div>
-        )
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      accounts: [],
+      show: false,
+      name: '',
+      email: '',
+      phoneno: '',
+      password: ''
     }
+  }
+  componentDidMount() {
+    Axios.get('https://emp-web-9a0cd.firebaseio.com/accounts.json').then((response) => {
+      console.log('Response Data', response.data);
+      let fetchedAccounts = [];
+      for (let key in response.data) {
+        fetchedAccounts.push({
+          ...response.data[key],
+          id: key
+        })
+      }
+      this.setState({
+        accounts: fetchedAccounts
+      })
+      console.log('fetched Accounts', fetchedAccounts);
+    }).catch(error => {
+      console.log('error', error);
+    })
+  }
+  deleteAccount(account) {
+    Axios.delete('https://emp-web-9a0cd.firebaseio.com/accounts/' + account.id + '/.json').then((response) => {
+      alert('deleted successfully')
+
+      let allAccounts = this.state.accounts;
+      let index = allAccounts.indexOf(account);
+      let newAccounts = allAccounts.splice(index, 1);
+      console.log('new Account', newAccounts);
+      console.log('neew account', allAccounts)
+      this.setState(
+        account = allAccounts
+      )
+
+    })
+      .catch((error => {
+        alert('Deletion failed')
+      }))
+  }
+  editAccount(account) {
+    this.setState({
+      name: account.name,
+      email: account.email,
+      phoneno: account.phoneno,
+      password: account.password,
+      show: !this.state.show,
+      id:account.id
+    })
+  }
+  handleClose() {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+  putData() {
+
+  }
+  updateaccountData() {
+    const {id,name,email,password,phoneno} =this.state;
+    const account ={name,email,password,phoneno};
+    console.log('Account data',account);
+  Axios.put('https://emp-web-9a0cd.firebaseio.com/accounts/' + id + '/.json',account).then((response)=>{
+    console.log('updated Successfully');
+    this.handleClose();
+  })
+  .catch((error)=>{
+console.log('error',error)
+  })
+  }
+
+  render() {
+    return (
+      <div>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">PhoneNo</th>
+              <th scope="col">Password</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.accounts.map((account) => {
+                return <tr key={account.id}>
+                  <td>{account.name}</td>
+                  <td>{account.email}</td>
+                  <td>{account.phoneno}</td>
+                  <td>{account.password}</td>
+                  <td>
+                    <button className='btn btn-danger' onClick={this.deleteAccount.bind(this, account)}>Delete</button>
+                    <button className='btn btn-info' onClick={this.editAccount.bind(this, account)}>Edit</button>
+                  </td>
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
+        <div>
+
+          <Modal show={this.state.show} onHide={this.handleClose.bind(this)}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body><div className="row">
+              <div className="col">
+                <input type="text" value={this.state.name} onChange={(event) => { this.setState({ name: event.target.value }) }} className="form-control" placeholder="Name" />
+              </div>
+              <div className="col">
+                <input type="text" value={this.state.email} onChange={(event) => { this.setState({ email: event.target.value }) }} className="form-control" placeholder="Email" />
+              </div>
+            </div>
+              <div className="row">
+                <div className="col">
+                  <input type="text" className="form-control" onChange={(event) => { this.setState({ phoneno: event.target.value }) }} value={this.state.phoneno} placeholder="Phone Number" />
+                </div>
+                <div className="col">
+                  <input type="password" className="form-control" onChange={(event) => { this.setState({ password: event.target.value }) }} value={this.state.password} placeholder="Password" />
+                </div>
+              </div></Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleClose.bind(this)}>
+                Close
+          </Button>
+              <Button variant="primary" onClick={this.updateaccountData.bind(this)}>
+                Save Changes
+          </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ViewAccount
